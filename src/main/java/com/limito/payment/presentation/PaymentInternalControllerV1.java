4141b560256a61d3ee6aab1363765597e55e273d
sk <sangkyung21@naver.com>
2025-12-08 11:06:10 +0900

    [refactor] 주문서비스로 부터 결제 승인요청을 처리하는 dto 수정

--- src/main/java/com/limito/payment/presentation/PaymentInternalControllerV1.java
+++ src/main/java/com/limito/payment/presentation/PaymentInternalControllerV1.java
@@ -11,10 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.limito.payment.application.PaymentServiceV1;
-import com.limito.payment.presentation.dto.request.ConfirmPaymentRequestV1;
+import com.limito.payment.presentation.dto.request.CreatePaymentRequestV1;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping("/internal/v1/payments")
 @RequiredArgsConstructor
@@ -25,7 +27,14 @@ public class PaymentInternalControllerV1 {
 	@PostMapping("/{orderId}/confirm")
 	public ResponseEntity<Object> confirmPayment(
 		@PathVariable String orderId,
-		@RequestBody ConfirmPaymentRequestV1 request) {
+		@RequestBody CreatePaymentRequestV1 request) {
+		try {
+			paymentService.validatePaymentRequestUniqueness(UUID.fromString(orderId));
+		} catch (Exception e) {
+			log.warn("주문 아이디 orderId={}에 대한 결제 요청 내역이 있습니다. {}", orderId, e.getMessage());
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("중복 결제 불가");
+		}
+		paymentService.createPayment(UUID.fromString(orderId), request);
 		return ResponseEntity.status(HttpStatus.OK).body(null);
 	}
 }
---
+++ src/main/java/com/limito/payment/presentation/dto/request/CreatePaymentRequestV1.java
@@ -0,0 +1,28 @@
+package com.limito.payment.presentation.dto.request;
+
+import java.util.List;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class CreatePaymentRequestV1 {
+	@NotNull(message = "주문 요약은 필수입니다.")
+	private String itemSummary;
+
+	@NotNull(message = "결제를 진행할 상품 목록은 필수입니다.")
+	private List<OrderItem> items;
+
+	@NotNull(message = "총 결제 금액은 필수입니다.")
+	private int totalPrice;
+}
